2192b71e907e7a574386bf625f933cd1884fea73
Ridhatama <ridhatama123@gmail.com>
2022-05-28 00:48:04 +0700

    UI, sound, finish

---
+++ Adventure of Udil/Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    private Material BlackMamba;
+    private Material matDefault;
+    public ItemCollector item;
+    public AudioSource doorsound;
+
+    private void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        BlackMamba = Resources.Load("BlackMamba", typeof(Material)) as Material;
+        matDefault = sr.material;
+    }
+    
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if(collision.gameObject.tag=="Player"){
+            if(Input.GetAxisRaw("Crouch") == 1 && item.Kunci){
+                sr.material = BlackMamba;
+                doorsound.Play();
+                Debug.Log("lele");
+            }
+        }
+    }
+}
--- Adventure of Udil/Assets/Scripts/ItemCollector.cs
+++ Adventure of Udil/Assets/Scripts/ItemCollector.cs
@@ -6,13 +6,24 @@ using TMPro;
 public class ItemCollector : MonoBehaviour
 {
     private int Diamond = 0;
+    public bool Kunci = false;
+    public AudioSource coinSound;
+    public AudioSource KunciSound;
     [SerializeField] private TextMeshProUGUI diamondcount;
+    [SerializeField] private TextMeshProUGUI kunci;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.CompareTag("Collectible")){
             Destroy(collision.gameObject);
             Diamond++;
+            coinSound.Play();
             diamondcount.text = "x " + Diamond;
+        }else if(collision.gameObject.CompareTag("Kunci")){
+            Destroy(collision.gameObject);
+            Kunci = true;
+            KunciSound.Play();
+            Debug.Log(Kunci);
+            kunci.text = "<sprite=0>";
         }
     }
 
